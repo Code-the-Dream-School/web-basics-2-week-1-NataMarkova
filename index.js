3c1fa8a6b660b0b278d0282f99090f3f95ef4afc
NataMarkova <47131166+NataMarkova@users.noreply.github.com>
2020-06-23 20:46:37 -0700

    Assignment (#2)
    * Add the results of working on the assignment #3: JavaScript Functions
    
    * Restructure the files within the assignment
    
    * Remove js folder
    
    * Redo according mentor's notes
    
    Co-authored-by: Natalia Markova <nataliamarkova@Admins-MacBook-Air.local>

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 
 console.log('hello world')
 
+
 /* Challenge #1
  * Create a variable and print the value
  *
@@ -19,7 +20,9 @@ console.log('hello world')
  * Example Output: n/a
  */
 
-// TODO: ADD CODE HERE
+const some_var = 3;
+console.log(some_var);
+
 
 /* Challenge #2
  * Create variables for your first and last name then print your full name
@@ -32,7 +35,10 @@ console.log('hello world')
  * Example Output: "Elizabeth Thompson"
  */
 
-// TODO: ADD CODE HERE
+const firstName = "Natalia";
+const lastName = "Markova";
+console.log(`${firstName} ${lastName}`);
+
 
 /* Challenge #3
  * Calculate the total price rounded to two decimals
@@ -46,7 +52,12 @@ console.log('hello world')
  * Example Output: "$49.90"
  */
 
-// TODO: ADD CODE HERE
+const price = 11.20;
+const quantity = 10;
+const total = price * quantity;
+const totalRounded = total.toFixed(2)
+console.log(totalRounded);
+
 
 /* Challenge #4
  * Declare a variable and conditionally assign it a value
@@ -62,7 +73,15 @@ console.log('hello world')
  * Example Output: "Aww! It's not sunny outside"
  */
 
-// TODO: ADD CODE HERE
+const weather = "Rainy";
+var message;
+if (weather === "Sunny") {
+    message = "Yay! It's sunny outside";
+} else {
+    message = "Aww! It's not sunny outside";
+}
+console.log(message);
+
 
 /* Challenge #5
  * Create a function that returns any string
@@ -75,7 +94,12 @@ console.log('hello world')
  * Example Output: "I called a function!"
  */
 
-// TODO: ADD CODE HERE
+function myFunction() {
+    const randStr = "This is my first function using JavaScript";
+    return randStr;
+}
+console.log(myFunction());
+
 
 /* Challenge #6
  * Rewrite the function in Challenge #5 as an arrow function
@@ -87,8 +111,10 @@ console.log('hello world')
  *
  * Example Output: "I called an arrow function!"
  */
+// implicit return w/o return keyword (possible using a single-line function)
 
-// TODO: ADD CODE HERE
+const myArrowFunction = () => "This is my first Arrow Function using JavaScript"
+console.log(myArrowFunction());
 
 /* Challenge #7
  * Create a function that accepts a string argument and returns it in all uppercase
@@ -97,12 +123,21 @@ console.log('hello world')
  * 1. Create a function called `stringToUpper`
  * 2. Add a parameter called `str` in your function signature
  * 3. Add a return statement in your function which returns the input string in all uppercase letters
- * 4. Use `console.log` to print out the output of `stringToUpper(str)`
+ * 4. Use `console.log` to print out the output of `stringToUpper(s
  *
  * Example Output: "WHY AM I SCREAMING"
  */
 
-// TODO: ADD CODE HERE
+//  Function Declaration:
+// function stringToUpper(str) {
+//     return str.toUpperCase();
+// }
+// console.log(stringToUpper("I'm almost done with my homework"));
+
+// Arrow Function:
+const stringToUpper = (str) => str.toUpperCase();
+console.log(stringToUpper("I'm almost done with my homework using arrow function"));
+
 
 /* Challenge #8
  * Create a function that accepts a number argument and returns double its value
@@ -119,4 +154,13 @@ console.log('hello world')
  * Example Output: 2, 10, 20
  */
 
-// TODO: ADD CODE HERE
+function multiplyByTwo(number) {
+    if (isNaN(number)) {
+        throw Error("An argument must be a number");
+    }
+    return number * 2;
+}
+var num1 = multiplyByTwo(1);
+var num2 = multiplyByTwo(5);
+var num3 = multiplyByTwo(10);
+console.log(`Output of the function is: ${num1}, ${num2}, ${num3}`);
